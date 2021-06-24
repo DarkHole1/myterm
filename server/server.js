4be7e2ba0aa8ff65a4304a0b97f0b4993f29aea4
Dark Hole <dark.hole1@yandex.ru>
2021-06-24 17:14:18 +0300

    Added connection backend

--- server/server.js
+++ server/server.js
@@ -8,22 +8,30 @@ const log = require('debug')('app:server');
 const TCPSocket = require('./socket');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://mongodb/myterm', {useNewUrlParser: true, useUnifiedTopology: true})
+const User = require('./user');
 
-app.use(require('./static'));
 app.use(require('./auth'));
+app.use(require('./static'));
 
 app.get('/terminals', (req, res) => {
   res.json(req.user.terminals.map(({ name }) => name));
 })
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   log('Client connected');
   let client = null;
 
+  const res = socker.handshake;
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [login, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+  const user = await User.findOne({ name: login, password: password });
+  let readonly = true;
 
-  socket.on('client', ([host, port]) => {
+  socket.on('client', ([id]) => {
     log('Connecting to remote');
     socket.emit('status', 'Connecting');
+    const { host, port } = user.terminals[id];
+    readonly = user.terminals[id].readonly;
 
     client = new TCPSocket(host, port, () => {
       log('Connected to remote');
@@ -48,6 +56,11 @@ io.on('connection', (socket) => {
   });
 
   socket.on('data', (data) => {
+    if(readonly) {
+      log('Data received, but connection readonly');
+      return;
+    }
+
     if(client === null) {
       log('Data received, but remote not connected');
       socket.emit('status', 'Disconnected');
