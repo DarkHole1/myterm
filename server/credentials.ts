2c11bdd09ca698de2b0f85085bf980240144eac2
Dark Hole <dark.hole1@yandex.ru>
2021-06-26 20:11:07 +0300

    Added class for credentials

---
+++ server/credentials.ts
@@ -0,0 +1,19 @@
+class Credentials {
+    private _name: string;
+    private _password: string;
+
+    constructor(name: string, password: string) {
+        this._name = name;
+        this._password = password;
+    }
+
+    getCredentials(): { name: string, password: string } {
+        return {name: this._name, password: this._password};
+    }
+
+    static fromBasicAuth(basicAuth?: string): Credentials {
+        const b64auth: string = (basicAuth || '').split(' ')[1] || '';
+        const [login, password]: string[] = Buffer.from(b64auth, 'base64').toString().split(':');
+        return new Credentials(login, password);
+    }
+}
