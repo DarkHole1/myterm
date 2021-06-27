f7bf77f8d4d4deceaaeea2931ea1ae166d5a7ddc
Dark Hole <dark.hole1@yandex.ru>
2021-06-27 16:51:43 +0300

    Some logging stuff

--- server/remotetcp.ts
+++ server/remotetcp.ts
@@ -1,4 +1,7 @@
 import net from 'net';
+import debug from 'debug';
+
+const log = debug('app:remote:tcp');
 
 class RemoteTCP {
     private _socket: net.Socket;
@@ -6,17 +9,21 @@ class RemoteTCP {
 
     constructor({ host, port, readonly }: { host: string, port: number, readonly: boolean }) {
         this._socket = new net.Socket();
-        this._socket.connect(port, host);
+        this._socket.connect(port, host, () => {
+            log('Connected to %s:%d', host, port);
+        });
         this._readonly = readonly;
     }
 
     attach(socket: any): void {
         this._socket.on('data', (data) => {
+            log('Data received from socket, sending to client');
             socket.emit('data', data);
         })
 
         if(!this._readonly) {
             socket.on('data', (data: any) => {
+                log('Data received from client, sending to socket');
                 this._socket.write(data);
             })
         }
