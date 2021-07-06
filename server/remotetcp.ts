81c67ff6a5fba058559698d3e5814afa52dc3799
Dark Hole <dark.hole1@yandex.ru>
2021-07-06 15:30:36 +0300

    Fix dup on multi term

--- server/remotetcp.ts
+++ server/remotetcp.ts
@@ -8,6 +8,7 @@ class RemoteTCP {
     private _readonly: boolean;
     private _id: string;
     private _attached: any;
+    private _history: any;
 
     constructor({ host, port, readonly }: { host: string, port: number, readonly: boolean }) {
         this._id = `${host}:${port}`;
@@ -38,7 +39,10 @@ class RemoteTCP {
     }
 
     history(history: any, length: number, drop: number): void {
+        this._history = history;
         log("History enabled %o", history);
+        let taken = !this._history['_'+this._id];
+        this._history['_'+this._id] = true;
         if(this._id in history) {
             log("Has history, writing to attached");
             this._attached.emit('data', history[this._id]);
@@ -47,6 +51,11 @@ class RemoteTCP {
             history[this._id] = Buffer.alloc(0);
         }
         this._socket.on('data', data => {
+            if(!taken) {
+                taken = !this._history['_'+this._id];
+                this._history['_'+this._id] = true;
+                if(!taken) return;
+            }
             history[this._id] = Buffer.concat([history[this._id], data]);
             if(history[this._id].length > length) {
                 history[this._id] = history[this._id].slice(drop);
@@ -55,6 +64,7 @@ class RemoteTCP {
     }
 
     close(): void {
+        this._history['_'+this._id] = false;
         this._socket.destroy();
     }
 }
