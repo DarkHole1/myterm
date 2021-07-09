e951e5b9b4e958d4c8f05c5c21ec953b148dc795
Dark Hole <dark.hole1@yandex.ru>
2021-07-09 23:15:29 +0300

    Add pretty design on main

--- client/index.js
+++ client/index.js
@@ -2,19 +2,25 @@
 const log = debug('app:frontend');
 
 const terminalsEl = document.getElementById('terminals');
+const spinnerEl = document.getElementById('spinner');
 // Fetch terminal list
 fetch('/terminals').then(res => res.json()).then(res => {
   log('Get %o', res);
   terminalsEl.innerHTML = '';
   for(const el of res) {
     const link = document.createElement('a');
-    link.innerText = el;
+    link.innerText = el + ' ';
     link.href = '/terminal.html#' + el;
     link.target = '_blank';
 
     const li = document.createElement('li');
     li.appendChild(link);
 
+    // Style
+    link.classList.add('nav-link', 'active');
+    li.classList.add('nav-item');
+
     terminalsEl.appendChild(li);
   }
+  spinnerEl.style.display = 'none';
 });
