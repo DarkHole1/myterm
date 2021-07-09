2e01652fe2fcab8b80a62be46579be04f24ef849
Dark Hole <dark.hole1@yandex.ru>
2021-07-09 18:29:26 +0300

    Remove old history

--- server/remotetcp.ts
+++ server/remotetcp.ts
@@ -13,12 +13,8 @@ type Params = {
 class RemoteTCP {
     private _socket: net.Socket;
     private _readonly: boolean;
-    private _id: string;
-    private _attached: any;
-    private _history: any;
 
     constructor({ host, socket, port, readonly }: Params) {
-        // TODO: Socket
         if(socket) {
             this._socket = socket;
         } else {
@@ -27,12 +23,10 @@ class RemoteTCP {
                 log('Connected to %s:%d', host, port);
             });
         }
-        this._id = `${host}:${port}`;
         this._readonly = readonly;
     }
 
     attach(socket: any): void {
-        this._attached = socket;
         this._socket.on('data', (data) => {
             log('Data received from socket, sending to client');
             socket.emit('data', data);
@@ -50,33 +44,7 @@ class RemoteTCP {
         })
     }
 
-    history(history: any, length: number, drop: number): void {
-        this._history = history;
-        log("History enabled %o", history);
-        let taken = !this._history['_'+this._id];
-        this._history['_'+this._id] = true;
-        if(this._id in history) {
-            log("Has history, writing to attached");
-            this._attached.emit('data', history[this._id]);
-        } else {
-            log("Has no history ;(");
-            history[this._id] = Buffer.alloc(0);
-        }
-        this._socket.on('data', data => {
-            if(!taken) {
-                taken = !this._history['_'+this._id];
-                this._history['_'+this._id] = true;
-                if(!taken) return;
-            }
-            history[this._id] = Buffer.concat([history[this._id], data]);
-            if(history[this._id].length > length) {
-                history[this._id] = history[this._id].slice(drop);
-            }
-        })
-    }
-
     close(): void {
-        this._history['_'+this._id] = false;
         this._socket.destroy();
     }
 }
