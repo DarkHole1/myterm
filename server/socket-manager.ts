7fa66fae70b84d447e9d03cb4374d6eb13457707
Dark Hole <dark.hole1@yandex.ru>
2021-07-17 16:38:49 +0300

    Add method for restarting sockets

--- server/socket-manager.ts
+++ server/socket-manager.ts
@@ -11,16 +11,22 @@ type SocketMap = {
         history: Buffer
     }
 };
-type Params = {
+type GetParams = {
     host: string,
     port: number,
     readonly: boolean,
     config: Config
 }
 
+type RestartParams = {
+    host: string,
+    port: number,
+    config: Config
+}
+
 class SocketManager {
     private static _sockets: SocketMap = {};
-    static get({ host, port, readonly, config }: Params): [RemoteTCP, Buffer] {
+    static get({ host, port, readonly, config }: GetParams): [RemoteTCP, Buffer] {
         const id = `${host}:${port}`;
         log('Data %o', this._sockets);
         if(!(id in this._sockets)) {
@@ -54,6 +60,31 @@ class SocketManager {
             }),
             this._sockets[id].history
         ];
+    }
+
+    static restart({ host, port, config }: RestartParams): void {
+        const id = `${host}:${port}`;
+        this._sockets[id].socket.destroy();
+        
+        const socket = new Socket;
+        socket.connect(port, host);
+
+        this._sockets[id].socket = socket;
+        if(config.recordSessions) {
+            socket.on('data', data => {
+                let { history } = this._sockets[id];
+                history = Buffer.concat([history, data]);
+                if(history.length > config.sessionBytesCount) {
+                    history = history.slice(config.dropBytes);
+                }
+                this._sockets[id].history = history;
+            });
+        }
+
+        socket.on('error', err => {
+            log("Error in socket: %o", err);
+            delete this._sockets[id];
+        });
     }
 }
 
