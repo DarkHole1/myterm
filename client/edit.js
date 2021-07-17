c930eeca81c46cc3752436dd643bc3fc2c846fcb
Dark Hole <dark.hole1@yandex.ru>
2021-07-17 15:40:34 +0300

    Fixed incorrect element naming

--- client/edit.js
+++ client/edit.js
@@ -4,7 +4,7 @@ const log = debug('app:frontend');
 const terminalId = decodeURI(location.hash.slice(1));
 fetch('/terminalsInfo').then(res => res.json()).then(res => {
     log('Get %o', res);
-    const terminal = res.filter(({ id }) => id == terminalId)[0];
+    const terminal = res.filter(({ _id }) => _id == terminalId)[0];
     const byId = document.getElementById.bind(document);
     byId('host').value = terminal.host;
     byId('port').value = terminal.port;
