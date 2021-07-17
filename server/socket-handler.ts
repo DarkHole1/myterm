51474436520e33b542d6581849d0e6c60b7277da
Dark Hole <dark.hole1@yandex.ru>
2021-07-17 14:30:55 +0300

    Add connecting from query params

--- server/socket-handler.ts
+++ server/socket-handler.ts
@@ -11,27 +11,21 @@ let history: any = {};
 export default function(io: any, config: Config): void {
     io.on('connection', async (socket: any) => {
         log('Client connected');
-        let client: RemoteTCP = null;
+        log('Terminal %o', socket.handshake.query.terminal);
       
         const user = await User.findByCredentials(
             Credentials.fromBasicAuth(socket.handshake.headers.authorization)
         );
+
+        log('Connecting to remote');
+        const terminalName = socket.handshake.query.terminal;
+        const { terminal: { host, port }, readonly } = user.terminals.filter((el: TerminalInfo) => el.terminal.name == terminalName)[0];
       
-        socket.on('client', (name: string) => {
-            if(client != null) {
-                log('Trying to reconnect, ignoring');
-                return;
-            }
-            log('Connecting to remote');
-            const { terminal: { host, port }, readonly } = user.terminals.filter((el: TerminalInfo) => el.terminal.name == name)[0];
-      
-            let history;
-            [client, history] = SocketManager.get({ host, port, readonly, config });
-            client.attach(socket);
-            log("Sessions %o", config.recordSessions);
-            if(config.recordSessions) {
-                socket.emit('data', history);
-            }
-        });
-      })
+        let [client, history] = SocketManager.get({ host, port, readonly, config });
+        client.attach(socket);
+        log("Sessions %o", config.recordSessions);
+        if(config.recordSessions) {
+            socket.emit('data', history);
+        }
+    })
 }
