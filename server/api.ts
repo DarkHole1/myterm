1ef4cc86213ed9135bd0e7774b146625e8370297
Dark Hole <dark.hole1@yandex.ru>
2021-08-08 19:54:11 +0300

    Implement listing terminals in api

--- server/api.ts
+++ server/api.ts
@@ -1,8 +1,23 @@
 import { Router } from 'express';
+import { TerminalInfo } from './user';
 const router = Router();
 
-router.get('/terminal.list', (req, res) => {
-    res.json([1, 2, 3]);
+router.get('/terminal.list', (req: any, res) => {
+    res.json(req.user.terminals.map((info: TerminalInfo) => {
+        let res = {
+            id: info.terminal._id,
+            name: info.terminal.name,
+            readonly: info.readonly,
+            editable: req.user.admin
+        };
+        if(req.user.admin) {
+            Object.assign(res, {
+                host: info.terminal.host,
+                port: info.terminal.port
+            })
+        }
+        return res;
+    }));
 })
 
 router.post('/terminal.update', (req, res) => {
