ebbd0c693cff057111417c72a7363caa5b15b2e0
Dark Hole <dark.hole1@yandex.ru>
2021-08-08 19:48:21 +0300

    MInor fix

--- server/api.ts
+++ server/api.ts
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 const router = Router();
 
-router.get('terminal.list', (req, res) => {
+router.get('/terminal.list', (req, res) => {
     res.json([1, 2, 3]);
 })
 
-router.post('terminal.update', (req, res) => {
+router.post('/terminal.update', (req, res) => {
     res.json({ success: false });
 })
 
-router.post('terminal.restart', (req, res) => {
+router.post('/terminal.restart', (req, res) => {
     res.json({ success: false });
 })
 
