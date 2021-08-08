2b4524276af93c24b6152317040bf90a9e96202d
Dark Hole <dark.hole1@yandex.ru>
2021-08-08 23:09:53 +0300

    Reverted back files deleted by accindent

---
+++ server/dist/terminal.js
@@ -0,0 +1,30 @@
+// Logging
+const log = debug('app:frontend');
+
+// Fit addon
+const fitAddon = new FitAddon.FitAddon();
+
+// Terminal initialization
+const term = new Terminal();
+term.loadAddon(fitAddon);
+term.open(document.getElementById('terminal'));
+
+fitAddon.fit();
+window.addEventListener('resize', e => {
+  fitAddon.fit();
+})
+
+const terminalName = decodeURI(location.hash.slice(1));
+
+// Socket.io initialization
+const socket = io({
+  query: {
+    terminal: terminalName
+  }
+});
+
+// Two-directional binding
+term.onData(data => socket.emit('data', data));
+socket.on('data', (data) => {
+  term.write(new Uint8Array(data));
+});
