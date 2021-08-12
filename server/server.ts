b8d2aac877333667b6180664564adbb5bd8164f9
Dark Hole <dark.hole1@yandex.ru>
2021-08-12 22:07:56 +0300

    Add ssl support

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,19 @@
 import express from 'express';
 import http from 'http';
+import https from 'https';
 import { Server } from "socket.io";
 import initializeSockets from './socket-handler';
 import AuthMiddleware from './auth';
 import StaticMiddleware from './static';
 import APIMiddleware from './api';
 import Config from './config';
+import init from './init-server';
 
 const config = new Config('.config.json');
 
 const app = express();
-const server = http.createServer(app);
+// const server = http.createServer(app);
+const server = init(app);
 const io = new Server(server);
 const log = require('debug')('app:server');
 import mongoose from 'mongoose';
