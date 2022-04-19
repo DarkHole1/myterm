b72b032dc75050165ed0e9461e08a5d58799e228
Dark Hole <dark.hole1@yandex.ru>
2022-04-19 13:17:26 +0300

    Fixed cookie auth

--- server/socket-handler.ts
+++ server/socket-handler.ts
@@ -8,23 +8,26 @@ const log = debug('app:socket');
 
 let history: any = {};
 
-export default function(io: any, config: Config): void {
+export default function (io: any, config: Config): void {
     io.on('connection', async (socket: any) => {
         log('Client connected');
         log('Terminal %o', socket.handshake.query.terminal);
-      
+
+        const cookies: any = Object.fromEntries(socket.handshake.headers.cookie.split('; ').map((e: any) => e.split('=')));
+        log('Cookies: %o', cookies);
         const user = await User.findByCredentials(
-            Credentials.fromBasicAuth(socket.handshake.headers.authorization)
+            // Credentials.fromBasicAuth(socket.handshake.headers.authorization)
+            Credentials.fromCookies(cookies)
         );
 
         log('Connecting to remote');
         const terminalId = socket.handshake.query.terminal;
         const { terminal: { host, port }, readonly } = await user.getTerminalById(terminalId);
-      
+
         let [client, history] = SocketManager.get({ host, port, readonly, config });
         client.attach(socket);
         log("Sessions %o", config.recordSessions);
-        if(config.recordSessions) {
+        if (config.recordSessions) {
             socket.emit('data', history);
         }
     })
