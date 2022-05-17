42cbc51df9185e115ffdc78d28d808f83636d569
Dark Hole <dark.hole1@yandex.ru>
2022-05-17 20:39:02 +0300

    Minor design change

--- svelte/src/components/Topbar.svelte
+++ svelte/src/components/Topbar.svelte
@@ -16,11 +16,13 @@
         display: flex;
         color: var(--font-color);
         font-size: 2rem;
-        font-weight: bold;
         border-bottom: 2px solid var(--block-highlight-color);
         padding: 0.5rem;
     }
     .fill {
         flex-grow: 1;
+    }
+    .title {
+        font-weight: bold;
     }
 </style>
