d6c8227c2298a59a0b44a835cc1d0dfd75c6727b
Dark Hole <dark.hole1@yandex.ru>
2022-12-01 21:56:50 +0300

    Add new api for finding or creating roles

--- svelte/src/api/roles.ts
+++ svelte/src/api/roles.ts
@@ -2,17 +2,29 @@ import { Readable, Subscriber, writable } from "svelte/store"
 import { z } from "zod"
 import API from "../API"
 
-const RawRole =
-    z.object({
-        id: z.string(),
-        name: z.string()
-    })
+const RawRole = z.object({
+    id: z.string(),
+    name: z.string()
+})
 
 type RawRole = z.infer<typeof RawRole>
+
+const Result = z.discriminatedUnion("success", [
+    z.object({
+        success: z.literal(true),
+        data: z.unknown()
+    }),
+    z.object({
+        success: z.literal(false)
+    })
+])
+
+type Result = z.infer<typeof Result>
 
 export class Role {
     private constructor(private data: RawRole) { }
 
+    // Converts from data
     static from(data: unknown) {
         return new this(RawRole.parse(data))
     }
@@ -21,23 +33,52 @@ export class Role {
         return z.array(RawRole).parse(data).map(e => new this(e))
     }
 
-    
-    public get name() : string {
+    public get name(): string {
         return this.data.name
     }
-    
 }
 
 export class Roles implements Readable<Role[]> {
+    roles!: Role[]
     $store = writable<Role[]>([])
 
     constructor() {
         setTimeout(() => this.update(), 0)
     }
 
+    async findOrCreate(name: string): Promise<Role> {
+        let role = this.findLocal(name)
+        if (role) {
+            return role
+        }
+        return await this.create(name)
+    }
+
+    private findLocal(name: string): Role | null {
+        for (const role of this.roles) {
+            if (role.name == name) {
+                return role
+            }
+        }
+
+        return null
+    }
+
+    async create(name: string): Promise<Role> {
+        const { data } = await API.$api.post<unknown>('/role.create', { name })
+        const res = Result.parse(data)
+        if (!res.success) {
+            throw 'Can\'t create role'
+        }
+
+        await this.update()
+        return Role.from(res.data)
+    }
+
     async update() {
         const { data } = await API.$api.get<unknown>('/role.list')
-        this.$store.set(Role.fromArray(data))
+        const roles = Role.fromArray(data)
+        this.$store.set(roles)
     }
 
     async rename(from: Role, to: string) {
