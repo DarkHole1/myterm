da33a7174c260fbe739505c3a55d5d00bb91bb20
Dark Hole <dark.hole1@yandex.ru>
2022-12-17 20:20:25 +0300

    Add class for folders

---
+++ svelte/src/api/folders.ts
@@ -0,0 +1,47 @@
+import { Readable, Subscriber, Unsubscriber, writable } from "svelte/store";
+import { z } from "zod";
+import API from "../API";
+
+const RawFolder = z.object({
+    id: z.string(),
+    name: z.string()
+})
+type RawFolder = z.infer<typeof RawFolder>
+
+export class Folder implements RawFolder {
+    public id: string
+    public name: string
+
+    private constructor(data: RawFolder) {
+        this.id = data.id
+        this.name = data.name
+    }
+
+    public static from(data: unknown) {
+        return new this(RawFolder.parse(data))
+    }
+
+    public static fromArray(data: unknown) {
+        return z.array(RawFolder).parse(data).map(e => new this(e))
+    }
+}
+
+export class Folders implements Readable<Folder[]> {
+    folders!: Folder[]
+    $store = writable<Folder[]>([])
+
+    constructor() {
+        setTimeout(() => this.update())
+    }
+
+    async update() {
+        const { data } = await API.$api.get<unknown>('/folders.list')
+        const folders = Folder.fromArray(data)
+        this.folders = folders
+        this.$store.set(folders)
+    }
+
+    subscribe(run: Subscriber<Folder[]>): Unsubscriber {
+        return this.$store.subscribe(run)
+    }
+}
