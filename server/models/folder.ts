7c38a45abc62e26d48e39d5d22fe98e70a743ae7
Dark Hole <dark.hole1@yandex.ru>
2022-12-17 18:14:20 +0300

    Fix ref

--- server/models/folder.ts
+++ server/models/folder.ts
@@ -2,12 +2,14 @@ import { DocumentType, getModelForClass, prop, Ref } from "@typegoose/typegoose"
 import { Terminal } from "./terminal"
 
 export class Folder {
-    @prop({ required: true })
+    @prop({ required: true, unique: true })
     public name!: string
 
-    @prop({ type: () => Terminal, default: [] })
+    @prop({ ref: () => Terminal, default: [] })
     public terminals!: Ref<Terminal>[]
 }
 
 export const FolderModel = getModelForClass(Folder)
 export type FolderModel = DocumentType<Folder>
+
+console.log(FolderModel.schema.obj)
