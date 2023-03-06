6526dcef06ff4f0bcb6937d58a4f5e183b1c42f9
Dark Hole <dark.hole1@yandex.ru>
2023-03-06 10:54:33 +0300

    Add method getting info

--- server/models/tv.ts
+++ server/models/tv.ts
@@ -9,6 +9,15 @@ export class TV {
 
     @prop({ required: true })
     public port!: number
+
+    getInfo(this: TVDocument) {
+        return {
+            id: this.id as string,
+            name: this.name,
+            host: this.host,
+            port: this.port
+        }
+    }
 }
 
 export type TVDocument = DocumentType<TV>
